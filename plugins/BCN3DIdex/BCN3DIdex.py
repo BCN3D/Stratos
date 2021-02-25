ff9ee9f5a54cc3069d19e2b5a31a6fc21c0408c4
Raúl Lázaro Sánchez <contacto@raullazaro.com>
2021-02-25 17:05:38 +0100

    finish change mode

--- plugins/BCN3DIdex/BCN3DIdex.py
+++ plugins/BCN3DIdex/BCN3DIdex.py
@@ -6,14 +6,10 @@ from typing import List, Optional, Any, Dict
 
 from UM.Extension import Extension
 from UM.Logger import Logger
-from UM.Settings.SettingDefinition import SettingDefinition
-from UM.Settings.DefinitionContainer import DefinitionContainer
-from UM.Settings.ContainerRegistry import ContainerRegistry
-from UM.Scene.Iterator.DepthFirstIterator import DepthFirstIterator
 
 from cura.CuraApplication import CuraApplication
-from cura.Scene.CuraSceneNode import CuraSceneNode
 from UM.i18n import i18nCatalog
+
 i18n_catalog = i18nCatalog("BCN3DIdex")
 
 
@@ -28,14 +24,6 @@ class BCN3DIdex(Extension):
 
         self._settings_dict = {}  # type: Dict[str, Any]
         self._expanded_categories = []  # type: List[str]  # temporary list used while creating nested settings
-
-        settings_definition_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "bcn3d_idex.def.json")
-        try:
-            with open(settings_definition_path, "r", encoding="utf-8") as f:
-                self._settings_dict = json.load(f, object_pairs_hook=collections.OrderedDict)
-        except:
-            Logger.logException("e", "Could not load bcn3d idex settings definition")
-            return
 
         self._onGlobalContainerStackChanged()
 
@@ -54,8 +42,16 @@ class BCN3DIdex(Extension):
             left_extruder = self._global_container_stack.extruderList[0]
             right_extruder = self._global_container_stack.extruderList[1]
 
+            try:
+                left_extruder.enabledChanged.disconnect(self._onEnabledChangedLeft)
+                right_extruder.enabledChanged.disconnect(self._onEnabledChangedRight)
+            except Exception:
+                # Just in case the connection didn't exists
+                pass
+
             self._application.getMachineManager().setExtruderEnabled(0, False)
             self._application.getMachineManager().setExtruderEnabled(1, False)
+
 
             if print_mode == "singleT0":
                 self._application.getMachineManager().setExtruderEnabled(0, True)
@@ -73,33 +69,42 @@ class BCN3DIdex(Extension):
                 self._application.getMachineManager().setExtruderEnabled(0, True)
                 self._application.getMachineManager().setExtruderEnabled(1, False)
 
+            left_extruder.enabledChanged.connect(self._onEnabledChangedLeft)
+            right_extruder.enabledChanged.connect(self._onEnabledChangedRight)
 
-
-
-
-
-            # if print_mode != "regular":
-            #     if not left_extruder.isEnabled:
-            #         # Force the left extruder to be enabled on mirror/duplication modes
-            #         self._application.getMachineManager().setExtruderEnabled(0, True)
-            #     self._application.getMachineManager().setExtruderEnabled(1, False)
-            #     right_extruder.enabledChanged.connect(self._onEnabledChanged)
-            #
-            #     for node in DepthFirstIterator(self._application.getController().getScene().getRoot()):
-            #         if not isinstance(node, CuraSceneNode) or not node.isSelectable():
-            #             continue
-            #         node.callDecoration("setActiveExtruder", left_extruder.getId())
-            # else:
-            #     try:
-            #         right_extruder.enabledChanged.disconnect(self._onEnabledChanged)
-            #     except Exception:
-            #         # Just in case the connection didn't exists
-            #         pass
-            #     self._application.getMachineManager().setExtruderEnabled(1, True)
-
-    def _onEnabledChanged(self):
+    def _onEnabledChangedLeft(self):
         print_mode = self._global_container_stack.getProperty("print_mode", "value")
-        if print_mode != "regular":
+        if print_mode == "singleT0":
+            left_extruder = self._global_container_stack.extruderList[0]
+            if not left_extruder.isEnabled:
+                self._application.getMachineManager().setExtruderEnabled(0, True)
+
+        elif print_mode == "singleT1":
+            self._global_container_stack.setProperty("print_mode", "value", "dual")
+
+        elif print_mode == "dual":
+            self._global_container_stack.setProperty("print_mode", "value", "singleT1")
+
+        else:
+            left_extruder = self._global_container_stack.extruderList[0]
+            if not left_extruder.isEnabled:
+                self._application.getMachineManager().setExtruderEnabled(0, True)
+
+    def _onEnabledChangedRight(self):
+        print_mode = self._global_container_stack.getProperty("print_mode", "value")
+
+        if print_mode == "singleT0":
+            self._global_container_stack.setProperty("print_mode", "value", "dual")
+
+        elif print_mode == "singleT1":
+            right_extruder = self._global_container_stack.extruderList[1]
+            if not right_extruder.isEnabled:
+                self._application.getMachineManager().setExtruderEnabled(1, True)
+
+        elif print_mode == "dual":
+            self._global_container_stack.setProperty("print_mode", "value", "singleT0")
+
+        else:
             right_extruder = self._global_container_stack.extruderList[1]
             if right_extruder.isEnabled:
                 # When in duplication/mirror modes force the right extruder to be disabled
