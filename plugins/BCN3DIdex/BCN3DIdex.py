a8459d433d0ee558725afdd01533eae5aa771855
Raúl Lázaro Sánchez <contacto@raullazaro.com>
2021-02-25 16:31:44 +0100

    initialize extruders

--- plugins/BCN3DIdex/BCN3DIdex.py
+++ plugins/BCN3DIdex/BCN3DIdex.py
@@ -54,6 +54,9 @@ class BCN3DIdex(Extension):
             left_extruder = self._global_container_stack.extruderList[0]
             right_extruder = self._global_container_stack.extruderList[1]
 
+            self._application.getMachineManager().setExtruderEnabled(0, False)
+            self._application.getMachineManager().setExtruderEnabled(1, False)
+
             if print_mode == "singleT0":
                 self._application.getMachineManager().setExtruderEnabled(0, True)
                 self._application.getMachineManager().setExtruderEnabled(1, False)
@@ -69,6 +72,7 @@ class BCN3DIdex(Extension):
             else:
                 self._application.getMachineManager().setExtruderEnabled(0, True)
                 self._application.getMachineManager().setExtruderEnabled(1, False)
+
 
 
 
