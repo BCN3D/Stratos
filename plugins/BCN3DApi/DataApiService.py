e99bd0f4412881afd3f71cf1590478ad7a00ed0d
alfonsopl <alfonsoplgu@gmail.com>
2021-10-07 14:26:21 +0200

    fix get printers

--- plugins/BCN3DApi/DataApiService.py
+++ plugins/BCN3DApi/DataApiService.py
@@ -6,8 +6,6 @@ from .http_helper import get, post
 
 
 class DataApiService:
-    #data_api_url = "https://api.bcn3d.com/data"
-    data_api_url = "http://api.astroprint.test/v2"
 
     def __init__(self):
         super().__init__()
@@ -21,13 +19,13 @@ class DataApiService:
         headers = {"Authorization": "Bearer {}".format(self._auth_api_service.getToken())}
         files = {"file": (gcode_path, open(gcode_path, "rb"))}
         data = {"setup": "{name : %s}" % gcode_name}
-        response = post(self.data_api_url + "/printfiles", data, headers, files)
+        response = post(self._auth_api_service.api_url + "/printfiles", data, headers, files)
         if 200 <= response.status_code < 300:
             response_message = response.json()
             print_file_id = response_message[0]["print_file_id"]
             data = {"print_file_id" : print_file_id }
             headers = {"Authorization": "Bearer {}".format(self._auth_api_service.getToken())}
-            response2 = post(self.data_api_url + "/devices/" + printerId + "/print", data, headers)
+            response2 = post(self._auth_api_service.api_url + "/devices/" + printerId + "/print", data, headers)
             if 200 <= response2.status_code < 300:
                 message = Message("The gcode has been sent to the cloud successfully", title="Gcode sent")
                 message.show()
@@ -41,7 +39,7 @@ class DataApiService:
 
     def getPrinters(self):
         headers = {"authorization": "bearer {}".format(self._auth_api_service.getToken()), 'Content-Type' : 'application/x-www-form-urlencoded'}
-        response = get(self.data_api_url + "/devices", headers=headers)
+        response = get(self._auth_api_service.api_url + "/devices", headers=headers)
         if 200 <= response.status_code < 300:
             return response.json()
         else:
@@ -49,7 +47,7 @@ class DataApiService:
 
     def getConnectedPrinter(self):
         headers = {"Authorization": "Bearer {}".format(self._auth_api_service.getToken())}
-        response = get(self.data_api_url + "/devices/connected", headers=headers)
+        response = get(self._auth_api_service.api_url + "/devices/connected", headers=headers)
         if 200 <= response.status_code < 300:
             return response.json()
         else:
